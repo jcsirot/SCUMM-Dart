af89762ef4eaba4e77f0c99e6594108d90e03ac2
Jean-Christophe Sirot <jcsirot@gmail.com>
2012-05-08 15:11:15 +0200

    Fix computing line width and minor text printing refactoring

--- src/Charset.dart
+++ src/Charset.dart
@@ -65,31 +65,15 @@ class Charset extends Resource {
     return w + dx;
   }
 
-  int getLineWidth(String str, [int start = 0]) {
-    int width = 1;
-    List<int> codes = str.charCodes();
-    Stream ins = new ScummFile.fromList(codes.getRange(start, codes.length - start));
-    while (!ins.eof()) {
-      int chr = ins.read();
-      if (chr == 0xff || chr == 0xfe) {
-        chr = ins.read();
-        if (chr == 2 || chr == 1 || chr == 9) {
-          break;
-        }
-      } else if (chr == 0xd) {
-        break;
-      }
-      width += getCharWidth(chr);
-    }
-    return width;
-  }
-
-  String addLineBreaks(String msg, int maxWidth) {
+  List<TextLine> splitString(String msg, int maxWidth) {
+    List<TextLine> lines = [];
     List<int> codes = msg.charCodes();
     int idx = 0;
     int chr;
     int curw = 1;
     int lastSpace = -1;
+    int lastWidth = 0;
+    int lastOffset = 0;
     while (idx < codes.length) {
       chr = codes[idx++];
       if (chr == 0x40) { /* @ */
@@ -98,31 +82,36 @@ class Charset extends Resource {
       if (chr == 0xfe || chr == 0xff) {
         chr = codes[idx++];
         if (chr == 1) {
-          codes[idx - 1] = 0xd;
+          lines.add(new TextLine(codes.getRange(lastOffset, idx-2-lastOffset), curw));
+          lastOffset = idx;
           curw = 1;
           continue;
         } else if (chr == 2) {
-          break;
+          lines.add(new TextLine(codes.getRange(lastOffset, idx-2-lastOffset), curw));
+          return lines;
         } else {
-          throw new Exception("addLineBreaks unsupported char code: ${chr}");
+          throw new Exception("splitString unsupported char code: ${chr}");
         }
       }
       if (chr == 0x20) { /* SPACE */
         lastSpace = idx - 1;
+        lastWidth = curw;
       }
       curw += getCharWidth(chr);
       if (lastSpace == -1) {
         continue;
       }
       if (curw > maxWidth) {
-        codes[lastSpace] = 0xd;
+        lines.add(new TextLine(codes.getRange(lastOffset, lastSpace-lastOffset), lastWidth));
         curw = 1;
         idx = lastSpace + 1;
+        lastOffset = idx;
         lastSpace = -1;
-        print("Break line, indexx=${lastSpace}");
+        print("Break line, index=${lastSpace}");
       }
     }
-    return new String.fromCharCodes(codes);
+    lines.add(new TextLine(codes.getRange(lastOffset, idx-lastOffset), curw));
+    return lines;
   }
 
   void printChar(int chr, int top, WritableStream out) {
@@ -162,34 +151,37 @@ class Charset extends Resource {
 
   void printString(Message msg, VirtualScreen vs) {
     vs.clearText();
-    String str = addLineBreaks(msg.value, 320);
-    int width = getLineWidth(msg.value);
-    int x = msg.params.center ? msg.params.x - (width >> 1) : msg.params.x;
-    int y = msg.params.y;
+    List<TextLine> lines = splitString(msg.value, 320);
     int oldColor = palette[1];
     palette[1] = msg.params.color;
-    Stream ins = new ScummFile.fromList(str.charCodes());
     WritableStream out = new ScummFile.fromUint8Array(vs.textBuffer);
-    int idx = 0;
-    while (!ins.eof()) {
-      int chr = ins.read();
-      if (chr >= 254) { // FIXME
-        idx++;
-        continue;
-      } else if (chr == 0xd) {
-        y += height;
-        width = getLineWidth(str, ++idx);
-        x = msg.params.center ? msg.params.x - (width >> 1) : msg.params.x;
-        out.reset();
-        out.seek(y * 320 + x);
-      } else {
-        out.reset();
-        out.seek(y * 320 + x);
-        x += getCharWidth(chr);
+    int x = 0;
+    int y = msg.params.y;
+    lines.forEach((TextLine line) {
+      Stream ins = new ScummFile.fromList(line.chars);
+      x = msg.params.center ? msg.params.x - (line.width >> 1) : msg.params.x;
+      int idx = 0;
+      while (!ins.eof()) {
+        int chr = ins.read();
+        if (chr >= 254) { // FIXME
+          ins.read();
+          continue;
+        } else {
+          out.reset();
+          out.seek(y * 320 + x);
+          x += getCharWidth(chr);
+        }
+        printChar(chr, y, out);
       }
-      idx++;
-      printChar(chr, y, out);
-    }
+      y += height;
+    });
     palette[1] = oldColor;
   }
+}
+
+class TextLine {
+  List<int> chars;
+  int width;
+
+  TextLine(this.chars, this.width);
 }
